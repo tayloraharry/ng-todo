30bd73964c52bc666b450fcc5021034f7b95336f
Taylor Harry <taylor.andrew.harry@gmail.com>
2016-11-15 19:33:47 -0600

    added day one to repo

--- javascripts/app.js
+++ javascripts/app.js
@@ -8,6 +8,7 @@ app.controller("NavCtrl", ($scope) => {
 
 app.controller("TodoCtrl", ($scope)=> {
   $scope.showListView = true;
+  $scope.newTask = {};
   $scope.items = [
     {
       id: 0,
@@ -26,6 +27,18 @@ app.controller("TodoCtrl", ($scope)=> {
       task:"take a nap",
       isCompleted:false,
       assignedTo:"Nate"
+    },
+    {
+      id: 2,
+      task:"take a nap",
+      isCompleted:false,
+      assignedTo:"Nate"
+    },
+    {
+      id: 2,
+      task:"take a nap",
+      isCompleted:false,
+      assignedTo:"Nate"
     }
   ];
   $scope.allItems = () => {
@@ -34,5 +47,16 @@ app.controller("TodoCtrl", ($scope)=> {
   $scope.newItem =  () => {
     $scope.showListView = false;
   }
+
+ $scope.addNewItem = function() {
+  $scope.newTask.isCompleted = false;
+  $scope.newTask.id = $scope.items.length;
+  $scope.items.push($scope.newTask);
+  $scope.newTask = {};
+  $scope.showListView = true;
+ }
+
+
+
 
 });
