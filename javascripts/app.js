34e1d599cfb3bb0fff14a624f62a03a2b65379dc
Taylor Harry <taylor.andrew.harry@gmail.com>
2016-11-15 18:11:12 -0600

    added day one progress

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,38 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp",[]);
 
+app.controller("NavCtrl", ($scope) => {
+  $scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Item"}];
+});
 
+app.controller("TodoCtrl", ($scope)=> {
+  $scope.showListView = true;
+  $scope.items = [
+    {
+      id: 0,
+      task:"mow the lawn",
+      isCompleted:true,
+      assignedTo:"Zoe"
+    },
+    {
+      id: 1,
+      task:"walk the dog",
+      isCompleted:false,
+      assignedTo:"Taylor"
+    },
+    {
+      id: 2,
+      task:"take a nap",
+      isCompleted:false,
+      assignedTo:"Nate"
+    }
+  ];
+  $scope.allItems = () => {
+    $scope.showListView = true;
+  }
+  $scope.newItem =  () => {
+    $scope.showListView = false;
+  }
+
+});
