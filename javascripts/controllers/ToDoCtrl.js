772256479a3aac32ddfccee137df8f97b484d460
Taylor Harry <taylor.andrew.harry@gmail.com>
2016-11-15 21:03:09 -0600

    pulled data from FB into app

--- javascripts/controllers/ToDoCtrl.js
+++ javascripts/controllers/ToDoCtrl.js
@@ -1,40 +1,14 @@
 "use strict";
 
-app.controller("TodoCtrl", ($scope)=> {
+app.controller("TodoCtrl", ($scope, ItemFactory)=> {
   $scope.showListView = true;
   $scope.newTask = {};
-  $scope.items = [
-    {
-      id: 0,
-      task:"mow the lawn",
-      isCompleted:true,
-      assignedTo:"Zoe"
-    },
-    {
-      id: 1,
-      task:"walk the dog",
-      isCompleted:false,
-      assignedTo:"Taylor"
-    },
-    {
-      id: 2,
-      task:"take a nap",
-      isCompleted:false,
-      assignedTo:"Nate"
-    },
-    {
-      id: 2,
-      task:"take a nap",
-      isCompleted:false,
-      assignedTo:"Nate"
-    },
-    {
-      id: 2,
-      task:"take a nap",
-      isCompleted:false,
-      assignedTo:"Nate"
-    }
-  ];
+  $scope.items = [];
+
+  ItemFactory.getItemList().then(function(items){
+   $scope.items = items;
+  })
+
   $scope.allItems = () => {
     $scope.showListView = true;
   };
